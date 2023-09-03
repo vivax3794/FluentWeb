9dd021b6a45d52ef920ab5a24476905af1ae76cb
vivax3794 <github@vivax3794.com>
2023-09-04 00:01:21 +0200

    hmmm

--- test_project/src/AppModule.rs
+++ test_project/src/AppModule.rs
@@ -8,10 +8,10 @@ pub struct Component {
     data: __Fluid_Data,
 }
 impl Component {
-    fn spawn_component___Fluent_UUID_bc7f35cd_646b_4199_8cfa_d700afd67a5c(&self) {
+    fn spawn_component___Fluent_UUID_e869af1f_2cfb_415f_bfa2_9127bea9cafa(&self) {
         let __Fluent_Elements = ::fluent_web_client::internal::get_elements(
             &self.root_name,
-            ".__Fluent_UUID_bc7f35cd_646b_4199_8cfa_d700afd67a5c.__Fluent_Needs_Init",
+            ".__Fluent_UUID_e869af1f_2cfb_415f_bfa2_9127bea9cafa.__Fluent_Needs_Init",
         );
         for __Fluent_Element in __Fluent_Elements.into_iter() {
             let __Fluent_Id = ::fluent_web_client::internal::uuid();
@@ -22,7 +22,7 @@ impl Component {
 }
 impl ::fluent_web_client::internal::Component for Component {
     fn render_init(&self) -> ::std::string::String {
-        "<p>Main</p>\n    <div class=\" __Fluent_UUID_bc7f35cd_646b_4199_8cfa_d700afd67a5c __Fluent_Needs_Init\"></div>\n<style>p{color:#00f}</style>"
+        "<p>Main</p>\n    <div class=\" __Fluent_UUID_e869af1f_2cfb_415f_bfa2_9127bea9cafa __Fluent_Needs_Init\"></div>\n<style>p{color:#00f}</style>"
             .into()
     }
     fn create(root_id: String) -> Self {
@@ -34,6 +34,6 @@ impl ::fluent_web_client::internal::Component for Component {
     fn setup_events(&self) {}
     fn update_all(&self) {}
     fn spawn_sub(&self) {
-        self.spawn_component___Fluent_UUID_bc7f35cd_646b_4199_8cfa_d700afd67a5c();
+        self.spawn_component___Fluent_UUID_e869af1f_2cfb_415f_bfa2_9127bea9cafa();
     }
 }
